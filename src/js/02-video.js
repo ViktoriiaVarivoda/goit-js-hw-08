f31b317faf02c0febadf0d32d867231a650442e2
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-24 19:49:41 +0300

    модуль8, задание2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+   player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 
 
