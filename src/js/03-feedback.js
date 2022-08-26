5dedd300d5bb33e28725d5277ef9827c149989bb
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-26 10:36:06 +0300

    дз8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,55 +1,33 @@
 import throttle from 'lodash.throttle';
-
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {}
-
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
 }
-
 refs.form.addEventListener('submit', onFormSubmit);
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
 refs.form.addEventListener('input', throttle(onFormInput, 500));
-
 populateTextarea();
 
 function onFormInput(e) {
 formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  // console.log(formData);
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-
-  // console.log('Отправляем форму');
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
  }
-
-// function onTextareaInput(evt) { 
-//   const massage = evt.target.value;
-//   localStorage.setItem(STORAGE_KEY, massage);
-// }
 
 function populateTextarea(evt) {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
-  
-//   if (savedMessage) {
-//     console.log(savedMessage);
-//     refs.textarea.value = savedMessage;
-//     const parsData = JSON.parse(savedMessage);
-//   console.log(parsData);
-//   }
-// }
-
  if (savedMessage) {
-   const parsData = JSON.parse(savedMessage);
-   console.log(parsData);
+    const parsData = JSON.parse(savedMessage);
     Object.entries(parsData).forEach(([name, value]) => {
       refs.form.elements[name].value = value;
-      });
+      console.log(parsData);
+    });
   }
 }
